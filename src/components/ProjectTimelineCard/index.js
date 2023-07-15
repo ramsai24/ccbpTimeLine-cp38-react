285ed16b949dcdcf1dde674aa766397dcf18a822
jnrsai24 <jnrsai24@gmail.com>
2023-07-15 10:41:13 +0000

    error in console and unable to render

--- src/components/ProjectTimelineCard/index.js
+++ src/components/ProjectTimelineCard/index.js
@@ -1,1 +1,24 @@
 // Write your code here
+import {AiFillClockCircle} from 'react-icons/ai'
+import './index.css'
+
+const ProjectTimelineCard = props => {
+  const {courseTitle, description, duration, tagsList} = props
+
+  return (
+    <div>
+      <div>
+        <h1>{courseTitle}</h1>
+        <AiFillClockCircle>{duration}</AiFillClockCircle>
+      </div>
+      <p>{description}</p>
+      <ul>
+        {tagsList.map(each => (
+          <li key={each.id}>{each}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default ProjectTimelineCard
--- src/components/TimelineView/index.js
+++ src/components/TimelineView/index.js
@@ -1,1 +1,43 @@
 // Write your code here
+
+import {Chrono} from 'react-chrono'
+import CourseTimelineCard from '../CourseTimelineCard'
+import ProjectTimelineCard from '../ProjectTimelineCard'
+import './index.css'
+
+const TimelineView = props => {
+  const {timelineItemsList} = props
+  console.log(timelineItemsList)
+
+  const renderSwitch = each => {
+    const {categoryId} = each
+    console.log(categoryId)
+    switch (categoryId) {
+      case 'COURSE':
+        return <CourseTimelineCard />
+      case 'PROJECT':
+        return <ProjectTimelineCard />
+      default:
+        return null
+    }
+  }
+
+  return (
+    <div>
+      <h1>MY JOURNEY OF</h1>
+      <h1>CCBP 4.0</h1>
+      <div>
+        {timelineItemsList.map(each => {
+          const {title} = each
+          return (
+            <Chrono key={each.id} mode="VERTICAL_ALTERNATING" items={title}>
+              {renderSwitch(each)}
+            </Chrono>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default TimelineView
